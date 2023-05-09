a95030c17a3b02c2ba2008b0e8534add6db4722c
indigold-code <111799252+indigold-code@users.noreply.github.com>
2023-05-09 11:34:51 -0500

    added a new model for tweets, a bunch of other features to user accounts and profiles

--- cloner/admin.py
+++ cloner/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from .models import Profile 
 # unregister groups
 admin.site.unregister(Group)
 
-
+# mix profile info into user info so not two separate pages
+class ProfileInline(admin.StackedInline):
+    model = Profile 
 
 # extend user model
 
@@ -12,10 +14,16 @@ class UserAdmin(admin.ModelAdmin):
     model = User
 
     fields = ["username"]
+    inlines = [ProfileInline]
 
 # unregister user 
 admin.site.unregister(User)
 
-# reregister user
+# reregister user and profile 
 
 admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
+
+
+
--- cloner/views.py
+++ cloner/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib import messages 
+from .models import Profile
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("You Must Be Logged In To Access This Page"))
+        return redirect('home')
+    
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST["follow"]
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            else:
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
+
+        return render(request, 'profile.html', {"profile":profile})
+    else:
+        messages.success(request, ("You Must Be Logged In To Access This Page"))
+        return redirect('home')
